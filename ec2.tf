c10fc3392b444a1c1d4edb57b5f86668af27b933
alfred <alfredshoetan@Alfreds-MacBook-Pro.local>
2022-06-02 21:19:41 -0400

    added SG & reference in ec2

--- ec2.tf
+++ ec2.tf
@@ -2,10 +2,12 @@ resource "aws_instance" "jenkins" {
     ami = var.my-ami-list["RHEL"]
     instance_type = var.my-instance-type[0]
 
-    #security_groups = 
+    security_groups = [aws_security_group.allow_ssh, aws_security_group.allow_web]
 
     tags = {
       "Name" = "Jenkins-Sandbox-TF"
       "Source" = "terraform"
     }
+
+    #userdata
 }
---
+++ security.tf
@@ -0,0 +1,54 @@
+resource "aws_security_group" "allow-ssh" {
+    name        = "allow_ssh"
+    description = "Allow SSH inbound traffic"
+    vpc_id = aws_vpc.main.id  
+  
+    ingress {
+        description      = "TLS from VPC"
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = [aws_vpc.main.cidr_block]
+        ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+    }
+
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    tags = {
+      "Name" = "allow-ssh"
+    }
+}
+
+
+resource "aws_security_group" "allow-web" {
+    name        = "allow_web"
+    description = "Allow Web inbound traffic on 8080"
+    vpc_id = aws_vpc.main.id  
+  
+    ingress {
+        description      = "TLS from VPC"
+        from_port        = 8080
+        to_port          = 8080
+        protocol         = "tcp"
+        cidr_blocks      = [aws_vpc.main.cidr_block]
+        ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+    }
+
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    tags = {
+      "Name" = "allow-jenkins-Web"
+    }
+}
